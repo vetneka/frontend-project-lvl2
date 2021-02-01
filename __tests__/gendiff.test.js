a7903037a1e5234cb637b00c2e6916abacdb276d
vetneka <vetneka@yandex.ru>
2021-02-01 14:22:41 +0500

    test: add test to compare flat of JSON files

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,30 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const filepath1 = getFixturePath('file1.json');
+const filepath2 = getFixturePath('file2.json');
+
+const diff = (
+  `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`
+);
+
+describe('compare flat files (JSON)', () => {
+  it('test 1', () => {
+    expect.hasAssertions();
+    expect(genDiff(filepath1, filepath2)).toStrictEqual(diff);
+  });
+});
