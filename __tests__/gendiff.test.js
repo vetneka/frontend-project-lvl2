c3da2f2a723e73ad42e3242b42908bea9a727a63
vetneka <vetneka@yandex.ru>
2021-02-05 18:07:02 +0500

    test: update fixtures and tests from plain-format to deep-format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,14 +8,50 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const diff = (
+const deepDiff = (
   `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        fee: 100500
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+    }
 }`
 );
 
@@ -26,7 +62,7 @@ describe('compare flat files (JSON)', () => {
     const filepath1 = getFixturePath('file1.json');
     const filepath2 = getFixturePath('file2.json');
 
-    expect(genDiff(filepath1, filepath2)).toStrictEqual(diff);
+    expect(genDiff(filepath1, filepath2)).toStrictEqual(deepDiff);
   });
 });
 
@@ -37,6 +73,6 @@ describe('compare flat files (YAML)', () => {
     const filepath1 = getFixturePath('file1.yml');
     const filepath2 = getFixturePath('file2.yml');
 
-    expect(genDiff(filepath1, filepath2)).toStrictEqual(diff);
+    expect(genDiff(filepath1, filepath2)).toStrictEqual(deepDiff);
   });
 });
