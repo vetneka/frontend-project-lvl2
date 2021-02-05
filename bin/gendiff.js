e82d9cc5bce1a4ed480d752c6e47a7ea63047f55
vetneka <vetneka@yandex.ru>
2021-02-05 18:14:45 +0500

    feat: add default param 'stylish'

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,9 +12,10 @@ program
   .version(VERSION)
   .description(DESCRIPTION)
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+    const formatName = program.opts().format;
+    const diff = genDiff(filepath1, filepath2, formatName);
     console.log(diff);
   });
 
