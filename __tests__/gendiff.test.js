62249c9fa51094c1132cf0140924b79dd89bdb7b
vetneka <vetneka@yandex.ru>
2021-02-07 21:54:56 +0500

    test: add test for plain formatter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -55,6 +55,20 @@ const deepDiff = (
 }`
 );
 
+const plainDiff = (
+  `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`
+);
+
 describe('compare flat files (JSON)', () => {
   it('test 1', () => {
     expect.hasAssertions();
@@ -74,5 +88,25 @@ describe('compare flat files (YAML)', () => {
     const filepath2 = getFixturePath('file2.yml');
 
     expect(genDiff(filepath1, filepath2)).toStrictEqual(deepDiff);
+  });
+});
+
+describe('plain formatter', () => {
+  it('json files', () => {
+    expect.hasAssertions();
+
+    const filepath1 = getFixturePath('file1.yml');
+    const filepath2 = getFixturePath('file2.yml');
+
+    expect(genDiff(filepath1, filepath2, 'plain')).toStrictEqual(plainDiff);
+  });
+
+  it('yml files', () => {
+    expect.hasAssertions();
+
+    const filepath1 = getFixturePath('file1.yml');
+    const filepath2 = getFixturePath('file2.yml');
+
+    expect(genDiff(filepath1, filepath2, 'plain')).toStrictEqual(plainDiff);
   });
 });
