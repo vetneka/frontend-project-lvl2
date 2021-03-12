ef155243ea509f175e21a11715ed9b003d8c7031
vetneka <vetneka@yandex.ru>
2021-03-12 20:33:58 +0500

    (review)refactor: change create prefix line algorithm for stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,83 +1,74 @@
 import _ from 'lodash';
 import nodeTypes from '../consts.js';
 
-const nodeStatusSymbol = {
-  added: '+',
-  removed: '-',
-  unchanged: ' ',
-  changed: ' ',
-  nested: ' ',
-};
-
 const spacesCount = 4;
 const placeholder = ' ';
 
-const createIndent = (depth, type = nodeStatusSymbol.unchanged) => {
-  const tempIndent = placeholder.repeat(spacesCount * depth);
-  const symbolIndex = tempIndent.length - 2;
-  const indent = Array.from(
-    tempIndent,
-    (item, index) => {
-      if (index === symbolIndex) {
-        return nodeStatusSymbol[type];
-      }
-      return item;
-    },
-  );
+const getNodeSymbol = (type) => {
+  switch (type) {
+    case nodeTypes.added:
+      return '+';
 
-  return indent.join('');
+    case nodeTypes.removed:
+      return '-';
+
+    default:
+      return placeholder;
+  }
 };
+
+const createPrefix = (depth) => placeholder.repeat((spacesCount * depth) - 2);
 
 const formatNodeValue = (value, depth) => {
   if (!_.isObject(value)) {
     return value;
   }
 
-  const indent = createIndent(depth);
+  const prefix = createPrefix(depth);
 
   const body = Object
     .keys(value)
-    .map((key) => `\n${indent} ${key}: ${formatNodeValue(value[key], depth + 1)}`)
+    .map((key) => `\n${prefix}${placeholder} ${key}: ${formatNodeValue(value[key], depth + 1)}`)
     .join('');
 
-  return `{${body}\n${createIndent(depth - 1)} }`;
+  return `{${body}\n${createPrefix(depth - 1)}  }`;
 };
 
-const getStylishLine = (node, depth, formatNodes) => {
+const getStylishLine = (node, depth) => {
   const {
     key, type, prevValue, nextValue, children,
   } = node;
-  const indent = createIndent(depth, type);
+  const prefix = createPrefix(depth);
+  const symbol = getNodeSymbol(type);
 
   switch (type) {
-    case nodeTypes.added:
-    case nodeTypes.removed:
-    case nodeTypes.unchanged:
-      return `\n${indent}${key}: ${formatNodeValue(prevValue, depth + 1)}`;
-
     case nodeTypes.changed: {
-      const indentRemove = createIndent(depth, 'removed');
-      const indentAdded = createIndent(depth, 'added');
+      const symbolRemove = getNodeSymbol(nodeTypes.removed);
+      const symbolAdded = getNodeSymbol(nodeTypes.added);
 
       return [
-        `\n${indentRemove}${key}: ${formatNodeValue(prevValue, depth + 1)}`,
-        `\n${indentAdded}${key}: ${formatNodeValue(nextValue, depth + 1)}`,
+        `\n${prefix}${symbolRemove} ${key}: ${formatNodeValue(prevValue, depth + 1)}`,
+        `\n${prefix}${symbolAdded} ${key}: ${formatNodeValue(nextValue, depth + 1)}`,
       ].join('');
     }
-    case nodeTypes.nested:
-      return `\n${indent}${key}: {${formatNodes(children, depth + 1)}\n${indent}}`;
+    case nodeTypes.nested: {
+      const formattedSubNodes = children
+        .map((subNode) => getStylishLine(subNode, depth + 1))
+        .join('');
 
+      return `\n${prefix}${symbol} ${key}: {${formattedSubNodes}\n${createPrefix(depth)}  }`;
+    }
     default:
-      throw new Error(`Unexpented node type: ${type}`);
+      return `\n${prefix}${symbol} ${key}: ${formatNodeValue(prevValue, depth + 1)}`;
   }
 };
 
 const formatToStylish = (diff) => {
-  const formatNodes = (nodes, depth) => nodes
-    .map((node) => getStylishLine(node, depth, formatNodes))
+  const body = diff
+    .map((node) => getStylishLine(node, 1))
     .join('');
 
-  return ['{', formatNodes(diff, 1), '\n}'].join('');
+  return ['{', body, '\n}'].join('');
 };
 
 export default formatToStylish;
